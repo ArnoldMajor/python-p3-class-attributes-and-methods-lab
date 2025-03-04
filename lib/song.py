2ba52f3fb4326704e5436c2ea84ae5386b6237b2
Arnold Mwangakala <arnoldmwangakala@gmail.com>
2025-03-04 16:19:47 +0300

    Complete lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,35 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        Song.add_song_to_count()
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_to_genres(self.genre)
+        Song.add_to_artists(self.artist)
+
+
+    @classmethod
+    def add_song_to_count (cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres (cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+        cls.add_to_genre_count(genre)
+
+    @classmethod
+    def add_to_artists (cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
+
+    @classmethod
+    def add_to_genre_count (cls, genre):
+        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
